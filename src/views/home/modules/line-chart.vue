32ebeebfe4f9ec530b747d1caab81fc9664b7918
逆流而上 <1666888816@qq.com>
2024-02-21 16:40:25 +0800

    增加订单分布q

--- src/views/home/modules/line-chart.vue
+++ src/views/home/modules/line-chart.vue
@@ -43,39 +43,39 @@ const { domRef, updateOptions } = useEcharts(() => ({
     type: 'value'
   },
   series: [
-    {
-      color: '#8e9dff',
-      name: $t('page.home.downloadCount'),
-      type: 'line',
-      smooth: true,
-      stack: 'Total',
-      areaStyle: {
-        color: {
-          type: 'linear',
-          x: 0,
-          y: 0,
-          x2: 0,
-          y2: 1,
-          colorStops: [
-            {
-              offset: 0.25,
-              color: '#8e9dff'
-            },
-            {
-              offset: 1,
-              color: '#fff'
-            }
-          ]
-        }
-      },
-      emphasis: {
-        focus: 'series'
-      },
-      data: [] as number[]
-    },
+    // {
+    //   color: '#8e9dff',
+    //   name: $t('page.home.downloadCount'),
+    //   type: 'line',
+    //   smooth: true,
+    //   stack: 'Total',
+    //   areaStyle: {
+    //     color: {
+    //       type: 'linear',
+    //       x: 0,
+    //       y: 0,
+    //       x2: 0,
+    //       y2: 1,
+    //       colorStops: [
+    //         {
+    //           offset: 0.25,
+    //           color: '#8e9dff'
+    //         },
+    //         {
+    //           offset: 1,
+    //           color: '#fff'
+    //         }
+    //       ]
+    //     }
+    //   },
+    //   emphasis: {
+    //     focus: 'series'
+    //   },
+    //   data: [] as number[]
+    // },
     {
       color: '#26deca',
-      name: $t('page.home.registerCount'),
+      name: '订单量',
       type: 'line',
       smooth: true,
       stack: 'Total',
@@ -116,8 +116,9 @@ async function mockData() {
         return item.order_date
       })
     // opts.xAxis.data = ['06:00', '08:00', '10:00', '12:00', '14:00', '16:00', '18:00', '20:00', '22:00', '24:00'];
-    opts.series[0].data = [4623, 6145, 6268, 6411, 1890, 4251, 2978, 3880, 3606, 4311];
-    opts.series[1].data = [2208, 2016, 2916, 4512, 8281, 2008, 1963, 2367, 2956, 678];
+    opts.series[0].data = props.userData.history.map(item=>{
+      return item.order_count
+    })
 
     return opts;
   });
