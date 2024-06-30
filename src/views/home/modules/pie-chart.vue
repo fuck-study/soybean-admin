14580b694a1e932ffc511e08a06f4184330f29ef
DokiDoki1103 <1666888816@qq.com>
2024-07-01 00:34:43 +0800

    修复了两个bug

--- src/views/home/modules/pie-chart.vue
+++ src/views/home/modules/pie-chart.vue
@@ -83,7 +83,7 @@ const {domRef, updateOptions} = useEcharts(() => ({
 
 async function mockData() {
   await new Promise(resolve => {
-    setTimeout(resolve, 2000);
+    setTimeout(resolve, 5000);
   });
   updateOptions(opts => {
     opts.series[0].data = Object.keys(props.userData.graph).map(item => {
